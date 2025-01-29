597fa10ef7e04087a612f7d496904b2bf8b02d7d
s29986-pj <s29986@pjwstk.edu.pl>
2025-01-29 17:09:48 +0100

    first commit

---
+++ shopee_s29986/src/main/java/com/example/shopee_s29986/ShopeeS29986Application.java
@@ -0,0 +1,13 @@
+package com.example.shopee_s29986;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeS29986Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeS29986Application.class, args);
+	}
+
+}
---
+++ shopee_s29986/src/test/java/com/example/shopee_s29986/ShopeeS29986ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopee_s29986;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeS29986ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
